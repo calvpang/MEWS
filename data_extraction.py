d9d12dbbf92a797d5d7464fb60f34981bafdd238
Nick <nickaswainston@gmail.com>
2022-10-01 13:49:18 +0800

    Included all compatible csvs

--- data_extraction.py
+++ data_extraction.py
@@ -43,19 +43,55 @@ for idx, row in df.iterrows():
     }, ignore_index=True)
 
 
-# df = pd.read_csv('Data/nakamura_1979_sm_locations.csv')
-# print (df)
-# for idx, row in df.iterrows():
-#     date_str = f'{row["Year"]}{row["Day"]:03d}{row["H"]:02d}{row["M"]:02d}{row["S"]:02d}'
-#     print(date_str)
-#     date = datetime.strptime(date_str, "%y%j%H%M%S")
-#     output_df = output_df.append({
-#         "Lat":row["Lat"],
-#         "Long":row["Long"],
-#         "Date":date,
-#         "Type":"sm",
-#         "Source": "nakamura_1979_sm_locations",
-#     }, ignore_index=True)
+df = pd.read_csv('Data/nakamura_1979_sm_locations.csv')
+print (df)
+for idx, row in df.iterrows():
+    date_str = f'{row["Year"]}{row["Day"]:03d}{row["H"]:02d}{row["M"]:02d}{row["S"]:02d}'
+    print(date_str)
+    date = datetime.strptime(date_str, "%Y%j%H%M%S")
+    output_df = output_df.append({
+        "Lat":row["Lat"],
+        "Long":row["Long"],
+        "Date":date,
+        "Type":"sm",
+        "Source": "nakamura_1979_sm_locations",
+    }, ignore_index=True)
+
+df = pd.read_csv('Data/nakamura_1983_ai_locations.csv')
+print (df)
+for idx, row in df.iterrows():
+    date_str = f'{row["Y"]}{row["JD"]:03d}{row["Hour"]:02d}{row["Min"]:02d}{int((str(row["Sec"])[:2])):02d}'
+    print(date_str)
+    date = datetime.strptime(date_str, "%Y%j%H%M%S")
+    output_df = output_df.append({
+        "Lat":row["Lat"],
+        "Long":row["Long"],
+        "Date":date,
+        "Type":"ai",
+        "Source": "nakamura_1983_ai_locations",
+    }, ignore_index=True)
+
+
+# Create a dictionary of cluster numbers that output their datetime
+cluster_num = {}
+df = pd.read_csv('Data/levent.1008weber.csv')
+for idx, row in df.iterrows():
+    datetime_str = f'{row["Y"]}{row["JD"]:03d}{row["S"]}'
+    this_datetime = datetime.strptime(datetime_str, "%y%j%H%M")
+    cluster_num[row["N2"]] = this_datetime
+
+
+df = pd.read_csv('Data/nakamura_2005_dm_locations.csv')
+print (df)
+for idx, row in df.iterrows():
+    date = cluster_num[row["A"]]
+    output_df = output_df.append({
+        "Lat":row["Lat"],
+        "Long":row["Long"],
+        "Date":date,
+        "Type":"dm",
+        "Source": "nakamura_2005_dm_locations",
+    }, ignore_index=True)
 
 print(output_df)
 output_df.to_csv("cleaned_data.csv")
